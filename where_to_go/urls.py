5729239804e239debca00e10fb4983c620c7d170
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-08-07 00:13:32 +0300

    Create places app

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import show_map
+from places.views import show_map
 from django.conf import settings
 from django.conf.urls.static import static
 
