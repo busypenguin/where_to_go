aa245ed9046c516357fe2fe75ca41b9289085660
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-08-07 22:06:31 +0300

    Revert "Add Place model"
    This reverts commit 8cb11b560a0092a89c3fc67a086a3a6c9e63074e.

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Place
 
-
-admin.site.register(Place)
+# Register your models here.
--- places/models.py
+++ places/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-
-class Place(models.Model):
-    place = models.CharField(max_length=200)
-
-    def __str__(self):
-        if self.place:
-            return self.place
+# Create your models here.
