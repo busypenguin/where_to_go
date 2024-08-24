44fcef4a3033c74414ee40b2e3e3adb844df32fb
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-08-24 17:04:10 +0300

    move Image to Place

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Place, Image
 
 
-admin.site.register(Place)
-admin.site.register(Image)
-# Register your models here.
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
