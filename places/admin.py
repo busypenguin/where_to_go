1d8ccb2a34b678bdc0dd89814fc2e41aee88dc47
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-08-26 21:57:40 +0300

    add preview photo

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.safestring import mark_safe
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["get_preview"]
+    fields = ('image', 'get_preview', 'number')
+
+    def get_prevview(self, obj):
+        return mark_safe('<img src="{url}" height=200 />'.format(
+            url=obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+            )
+        )
 
 
 @admin.register(Place)
