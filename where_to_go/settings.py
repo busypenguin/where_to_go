9a64a443dd81249a20cc5b7747643377a2d68851
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-10-03 14:07:18 +0300

    add ALLOWED_HOSTS to .env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 INSTALLED_APPS = [
