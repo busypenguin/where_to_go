5d38355d9151f21f3eb6796ea24849c1c0cc0821
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-10-04 18:45:14 +0300

    change imports

--- places/views.py
+++ places/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from .models import Place, Image
+from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 
