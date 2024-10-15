a98ec7968b373f927267fe6ddb29f5a55907977d
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-10-15 14:22:04 +0300

    change field names

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(verbose_name='Название', max_length=200)
-    description_short = models.TextField(verbose_name='Краткое описание', blank=True)
-    description_long = HTMLField(verbose_name='Полное описание', blank=True)
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
+    long_description = HTMLField(verbose_name='Полное описание', blank=True)
     lng = models.FloatField(verbose_name='Долгота', null=True)
     lat = models.FloatField(verbose_name='Широта', null=True)
 
