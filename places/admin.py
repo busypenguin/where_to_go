92c361cd500b68fbab053d310c1d06146e85620e
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-11-21 13:39:02 +0300

    change mark_safe to format_html

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Place, Image
 from django.utils.safestring import mark_safe
 from adminsortable2.admin import SortableTabularInline, SortableAdminMixin
+from django.utils.html import format_html
 
 
 class ImageInline(SortableTabularInline):
@@ -10,12 +11,13 @@ class ImageInline(SortableTabularInline):
     fields = ('image', 'get_preview', 'number')
 
     def get_preview(self, obj):
-        return mark_safe('<img src="{url}" height=200 />'.format(
-            url=obj.image.url,
-            width=obj.image.width,
-            height=obj.image.height,
-            )
-        )
+        return format_html('<img src="{url}" height=200 />',
+                           mark_safe(
+                            url=obj.image.url,
+                            width=obj.image.width,
+                            height=obj.image.height,
+                            )
+                           )
 
 
 @admin.register(Place)
