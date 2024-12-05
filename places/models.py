d56a6440e9918b3b4210f959b39b47972458d736
busypenguin <busy-penguin@yandex.ru>
2024-12-05 14:58:03 +0300

    make the field required

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField(verbose_name='Название', max_length=200)
     short_description = models.TextField(verbose_name='Краткое описание', blank=True)
     long_description = HTMLField(verbose_name='Полное описание', blank=True)
-    lng = models.FloatField(verbose_name='Долгота', null=True)
-    lat = models.FloatField(verbose_name='Широта', null=True)
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
 
     class Meta:
         ordering = ['title']
@@ -18,8 +18,8 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, verbose_name='Место',  related_name='images', on_delete=models.CASCADE, null=True)
-    image = models.ImageField(verbose_name='Изображение', null=True)
+    place = models.ForeignKey(Place, verbose_name='Место',  related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name='Изображение')
     number = models.PositiveIntegerField(verbose_name='Номер', default=0, db_index=True, blank=True)
 
     class Meta:
