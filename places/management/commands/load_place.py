dbc093be88fd47ac076a1bc4ea17aff15569511e
busypenguin <busy-penguin@yandex.ru>
2025-01-15 22:37:40 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,21 +14,23 @@ from places.models import Place, Image
 class Command(BaseCommand):
     help = 'Load places to DB'
 
+    def add_arguments(self, parser):
+        parser.add_argument('path_to_json', type=str, help='Путь до json файла')
+
     def handle(self, *args, **options):
-        path = 'places/places'
-        for dirs, folder, files in os.walk(path):
-            for file in files:
-                with open(f'{dirs}/{file}') as f:
-                    serialized_place = json.load(f)
-                    place, _ = Place.objects.get_or_create(
-                        title=serialized_place['title'],
-                        short_description=serialized_place['description_short'],
-                        long_description=serialized_place['description_long'],
-                        lng=serialized_place['coordinates']['lng'],
-                        lat=serialized_place['coordinates']['lat']
-                    )
-                    images = serialized_place['imgs']
-                    for index, img in enumerate(images):
-                        resp = requests.get(img)
-                        image, _ = Image.objects.get_or_create(place=place, number=index)
-                        image.image.save(str(img), ContentFile(resp.content), save=True)
+        path_to_json = options['path_to_json']
+        requested_path_to_json = requests.get(path_to_json)
+        serialized_place = requested_path_to_json.json()
+
+        place, _ = Place.objects.get_or_create(
+            title=serialized_place['title'],
+            short_description=serialized_place['description_short'],
+            long_description=serialized_place['description_long'],
+            lng=serialized_place['coordinates']['lng'],
+            lat=serialized_place['coordinates']['lat']
+        )
+        images = serialized_place['imgs']
+        for index, img in enumerate(images):
+            resp = requests.get(img)
+            image, _ = Image.objects.get_or_create(place=place, number=index)
+            image.image.save(str(img), ContentFile(resp.content), save=True)        
